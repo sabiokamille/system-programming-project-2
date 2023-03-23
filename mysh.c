ecc457220f35258f07f4dc5a5d09b6aed0e4b77d
Kamille Sabio <ks1510@ilab4.cs.rutgers.edu>
2023-03-23 19:24:24 -0400

    add initial mysh source code

---
+++ mysh.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+//function prototypes
+void interactive_mode();
+void batch_mode(char **argument);
+void prompt(int exit_status);
+void parse_input(char **next_line);
+void tokenize(char *line);
+void find_file(char *file_name);
+
+int main(int argc, char **argv)
+{
+	//check how many arguments the user gives
+	if (argc < 0){
+		perror("Usage: ./mysh.c <argument>*");
+		exit(-1);
+	} else if (argc == 1){
+		interactive_mode();
+	} else if (argc == 2){
+		batch_mode(argv);
+	}
+}
+
+void interactive_mode(){
+	prompt(1);
+}
+
+void batch_mode(char **arguments){
+	printf("now in batch mode\n");
+}
+
+void prompt(int exit_mode){
+	if(exit_mode != 0){
+		printf("!mysh> ");
+	} else{
+		printf("mysh> ");
+	}
+}
+
+void parse_input(char **next_line){
+
+}
+
+void tokenize(char* line){
+
+}
+
+void find_file(char *file_name){
+
+}
